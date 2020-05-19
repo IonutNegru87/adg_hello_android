7b8d8733cb50106b59bf5cc0bd5ee8f20ca267f8
inegru <inegru@cisco.com>
2020-05-19 07:23:27 +0300

    Add splash screen

--- app/src/main/java/com/inegru/siit/myapplication/SplashScreen.java
+++ app/src/main/java/com/inegru/siit/myapplication/SplashScreen.java
@@ -8,13 +8,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.inegru.siit.myapplication.week5.ImplicitIntentActivity;
 
+// Usually this intermediate activity is not needed (we can apply the splash theme on any starting activity),
+// but for this course it is useful because we can redirect to the desired course without moving the launcher
+// attributes from manifest
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // If we don't want to apply the theme in manifest we can do it programmatically
+        // Be sure to call this before onCreate()
+//        setTheme(R.style.SplashScreenTheme);
         super.onCreate(savedInstanceState);
+
+        // No need to use setContentView() here as the actual image
+        // is set directly on the window by the theme
 
         // Redirect to the desired activity
         startActivity(new Intent(this, ImplicitIntentActivity.class));
+
+        // Close splash screen so that it doesn't stay in back stack
+        finish(); // if we don't call this the user can actually go back to splash activity
     }
 }
